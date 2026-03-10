04e3dbbf8799b94b2eaa61ac29c10936b4df8287
manchikalapudinehru <nehrusai36@gmail.com>
2026-03-10 09:18:15 +0530

    first commit

---
+++ src/main/java/com/klef/fsad/exam/BookingHQLDemo/App.java
@@ -0,0 +1,9 @@
+package com.klef.fsad.exam;
+
+public class App 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hibernate Project Started");
+    }
+}
---
+++ src/main/java/com/klef/fsad/exam/BookingHQLDemo/ClientDemo.java
@@ -0,0 +1,47 @@
+package com.klef.fsad.exam;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class ClientDemo 
+{
+public static void main(String[] args) 
+{
+
+Configuration cfg = new Configuration();
+cfg.configure("hibernate.cfg.xml");
+
+SessionFactory sf = cfg.buildSessionFactory();
+Session session = sf.openSession();
+
+Transaction tx = session.beginTransaction();
+
+Booking b1 = new Booking("Sai","2026-03-10","Confirmed");
+Booking b2 = new Booking("Ravi","2026-03-12","Pending");
+
+session.save(b1);
+session.save(b2);
+
+tx.commit();
+
+System.out.println("Records Inserted");
+
+Query q = session.createQuery("from Booking");
+
+List<Booking> list = q.list();
+
+for(Booking b : list)
+{
+System.out.println(b.getId()+" "+b.getName()+" "+b.getDate()+" "+b.getStatus());
+}
+
+session.close();
+sf.close();
+
+}
+}
